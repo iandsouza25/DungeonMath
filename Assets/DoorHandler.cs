9a7e4468851022ecc3fb869ce9c9f2bb3d0ec957
Zhang-Sonny <sonnyzhang007@gmail.com>
2024-12-12 20:11:19 -0500

    door handler outlined script

--- Assets/DoorHandler.cs
+++ Assets/DoorHandler.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DoorHandler : MonoBehaviour
 {
+    public GameObject canvas
+    private bool canvasActive = false;
     // wip
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (canvas != null)
+        {
+            canvas.SetActive(false);  
+        }
     }
 
     // Update is called once per frame
-    void Update()
+     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))  
+        {
+            if (canvas != null) {
+                canvasActive = !canvasActive;  
+                canvas.SetActive(canvasActive);  
+            }
+        }
     }
 }
