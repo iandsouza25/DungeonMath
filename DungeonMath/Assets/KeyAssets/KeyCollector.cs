615f189d86608b04c2413106c34dd33aaef3fdde
Ian D'Souza <dsouza.ianp@gmail.com>
2024-12-15 17:24:38 -0500

    idk

--- DungeonMath/Assets/KeyAssets/KeyCollector.cs
+++ DungeonMath/Assets/KeyAssets/KeyCollector.cs
@@ -34,8 +34,8 @@ public class KeyCollector : MonoBehaviour
 
         eqGenerator.GenerateEquation();
         List<string> missingKeys = eqGenerator.GetMissingKeys();
-        //Debug.Log(eqGenerator.GetEquation());
-        //Debug.Log(string.Join(", ", eqGenerator.GetMissingKeys()));
+        // Debug.Log(eqGenerator.GetEquation());
+        // Debug.Log(string.Join(", ", eqGenerator.GetMissingKeys()));
         int currentLevel = eqGenerator.currentLevel;
 
 
